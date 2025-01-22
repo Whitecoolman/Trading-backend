807c517c9b28953cf168ed68e4c23d8558325fe4
miraclerose0303 <miraclerose0303@gmail.com>
2025-01-22 22:08:57 +0100

    tgtoken

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const puppeteer = require("puppeteer");
 const TelegramBot = require("node-telegram-bot-api");
+
+const app = express();
+app.use(bodyParser.json());
+
+// Telegram bot setup
+const TELEGRAM_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN';
+const CHAT_ID = 'YOUR_TELEGRAM_CHAT_ID';
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: false });
