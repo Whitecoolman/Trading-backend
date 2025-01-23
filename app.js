d4cb710a540137e1681a82502e01686f441c3491
miraclerose0303 <miraclerose0303@gmail.com>
2025-01-23 13:21:47 +0200

    alert process

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const bodyParser = require("body-parser");
 const puppeteer = require("puppeteer");
 const TelegramBot = require("node-telegram-bot-api");
 
+const app = express();
+app.use(bodyParser.json());
+
+const TELEGRAM_TOKEN = "token";
+const CHAT_ID = "chatID";
+const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: false});
+
+app.post('/webhook', async(req,res) =>{
+  const {symbol, volume, avg_volume_15, avg_volume_1h} = req.body;
+
+  if(volume > 1.5 * avg_volume_15 || volume > 1.5 * avg_volume_1h){
+    try{
+      const screenshotPath = await captureTradingViewChart(symbol);
+      await sendTelegramAlert(symbol, volume, screenshotPath);
+      res.status(200).send({status: "alert processed"});
+    }
+  }
+})
